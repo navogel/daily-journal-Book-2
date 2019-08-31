de05e89b5d631a81508a0b1ac407149ae4d29c25
Nate V <navogel@gmail.com>
2019-08-31 03:54:56 -0500

    made styling changes

--- src/scripts/events.js
+++ src/scripts/events.js
@@ -7,6 +7,30 @@ function showMe() {
 	}
 }
 
+function getLarge() {
+	var x = document.getElementById("height");
+	x.style.transform = "scale(1.8)";
+	x.style.transition = ".5s";
+	x.style.backgroundColor = "#f38f00";
+	x.style.borderRadius = "7px";
+	x.style.color = "white";
+}
+
+function getSmall() {
+	var x = document.getElementById("height");
+	x.style.transform = "scale(1)";
+	x.style.transition = ".5s";
+	x.style.backgroundColor = "white";
+	x.style.borderRadius = "10px";
+	x.style.color = "rgb(106, 133, 190)";
+}
+
+function clearContents(element) {
+	element.value = "";
+}
+
+// color for orange #ffb900  color for card bg #151b3d
+
 // document.querySelector(".button1").addEventListener("click", event => {
 // 	let dateInput = document.querySelector("#journalDate").value;
 // 	let moodInput = document.querySelector("#mood").value;
