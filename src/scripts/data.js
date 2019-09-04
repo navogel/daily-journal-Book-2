2da78c05e5a6db0d01e0195c511fd4f3f5f4f924
Nate V <navogel@gmail.com>
2019-09-03 21:35:43 -0500

    commented out index and data files

--- src/scripts/data.js
+++ src/scripts/data.js
@@ -1,11 +1,13 @@
 //API Station
 
 const API = {
+	//fetch json data and parse
 	getJournalEntries: () => {
 		return fetch("http://localhost:3000/journalArray").then(response =>
 			response.json()
 		);
 	},
+	//save submitted entry to DB
 	saveJournalEntry: entry => {
 		fetch("http://localhost:3000/journalArray", {
 			method: "POST",
